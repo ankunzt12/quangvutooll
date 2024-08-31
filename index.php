44a70156773972e750ef44dd71812ccff930dc8e
ankunzt12 <131801317+ankunzt12@users.noreply.github.com>
2024-09-01 00:37:43 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<body>
+<br>
+<br>
+<br>
+<br>
+<b>
+<span style="font-family: Impact">
+<h1><center><span style="font-size:90px">THANK YOU</span></center></h1>
+</span>
+</b>
+<br>
+<center>
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F41%2F2b%2F43%2F412b437c073e7092bdc0e3b9d7b49bd7.gif&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F822892163157986420%2F&tbnid=2Et1c8taaTMOUM&vet=12ahUKEwic46vD_-j1AhWxZmwGHYNCBKIQMygAegUIARDXAQ..i&docid=UsA3bGYdQ1ZB5M&w=474&h=266&q=gif%20anime%204k%20your%20name&client=ms-android-xiaomi-rvo3&ved=2ahUKEwic46vD_-j1AhWxZmwGHYNCBKIQMygAegUIARDXAQ" style=“width:250px;height:250px;”>
+</center>
+<br>
+<br>
+<span style="font-family: Comic Sans MS">
+<span style="font-size:24px">
+<?php
+ 
+        $key = $_GET['key'];
+        
+        
+        
+       
+
+ echo "<center>";
+ echo "Xin Chào! Cảm Ơn Bạn Đã Vượt Link Rút Gọn. Hãy Cùng Nhau Phát Triển Cộng Đồng Nhé"; echo "<br>";
+ echo "Key 1 ngày đổi 1 lần"; echo "<br>";
+ echo "API Key Ngày ";
+ echo date('d/m/Y');
+ echo " Là: "; echo "<br><b>";
+ echo $key; echo "</b>";
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
+echo "</center>";
+?>
+</span>
+</span>
+<span style="font-family: Comic Sans MS">
+<h><center><span style="font-size:15px">Copyright ©2022 | Nguyễn Đức Phát</span></center></h>
+</span>
+</body>
+
+</html>
